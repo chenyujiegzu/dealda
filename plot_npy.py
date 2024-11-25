f9292dfe1f6038f96e4b57bbaa8dcc2cd70b6365
yjchen <149309880+chenyujiegzu@users.noreply.github.com>
2024-11-25 19:07:17 +0800

    Update plot_npy.py

--- plot_npy.py
+++ plot_npy.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 加载 .npy 文件
+# loading the *.npy file
 avspec = np.load('combined_spectrum.npy')
 
-# 打印数组的形状和部分数据，检查它的内容
+# print and checking
 print(f"Shape of the spectrum: {avspec.shape}")
 print(f"Spectrum data (first 10 values): {avspec[:10]}")
 
-# 绘制频谱
+# plot
 plt.plot(avspec)
-plt.yscale('log')  # 如果数据差异很大，可以使用对数坐标
+plt.yscale('log')
 plt.xlabel('Frequency Channel')
 plt.ylabel('Amplitude')
 plt.title('Combined Spectrum')
