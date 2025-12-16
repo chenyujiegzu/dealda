67c35b8f1c4692028f611368127afc49d230bb81
Yujie Chen <149309880+Yu-Jie-Chen@users.noreply.github.com>
2025-12-16 22:44:40 +0800

    Update single_search.sh

--- transientx/single_search.sh
+++ transientx/single_search.sh
@@ -51,7 +51,7 @@ for day in "${dates[@]}"; do
     mkdir -p "${day}"
     chmod 777 "${day}"
 
-    echo transientx_fil -v -o "${day}/${day}_NGC4147" -t "${thread}" --zapthre "${zapthre}" --dms "${dms}" --ddm "${ddm}" --ndm "${ndm}" --thre "${thre}" --minw "${minw}" --maxw "${maxw}" -l "${lval}" --drop -z "${Z_RFI}" --psrfits "${data_dir}/${day}/${fits_pattern}" >> "$transientx_commands"
+    echo transientx_fil -v -o "${day}/${srcname{" -t "${thread}" --zapthre "${zapthre}" --dms "${dms}" --ddm "${ddm}" --ndm "${ndm}" --thre "${thre}" --minw "${minw}" --maxw "${maxw}" -l "${lval}" --drop -z "${Z_RFI}" --psrfits "${data_dir}/${day}/${fits_pattern}" >> "$transientx_commands"
 done
 
 echo "Running transientx_fil..."
@@ -67,7 +67,7 @@ echo "transientx_fil finished."
 echo "Generating replot_fil commands..."
 
 for day in "${dates[@]}"; do
-    echo replot_fil -v -t "${thread}" --zapthre "${zapthre}" --srcname "${srcname}" --ra "${RA}" --dec "${DEC}" --telescope "${telescope}" --dmcutoff "${dmcutoff}" --widthcutoff "${widthcutoff}" --snrcutoff "${snrcutoff}" --snrloss "${snrloss}" --zap --zdot --kadane ${kadane} --candfile "${day}/${day}*.cands" --clean --psrfits "${data_dir}/${day}/${fits_pattern}" >> "$replot_commands"
+    echo replot_fil -v -t "${thread}" --zapthre "${zapthre}" --srcname "${srcname}" --ra "${RA}" --dec "${DEC}" --telescope "${telescope}" --dmcutoff "${dmcutoff}" --widthcutoff "${widthcutoff}" --snrcutoff "${snrcutoff}" --snrloss "${snrloss}" --zap --zdot --kadane ${kadane} --candfile "${day}/*.cands" --clean --psrfits "${data_dir}/${day}/${fits_pattern}" >> "$replot_commands"
 done
 
 echo "Running replot_fil..."
