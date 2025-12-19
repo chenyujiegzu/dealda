ed23f495e89fbcbdea45a3c0621f6bcb898059b6
Yujie Chen <149309880+Yu-Jie-Chen@users.noreply.github.com>
2025-12-19 23:34:15 +0800

    Update run_dspsr.sh

--- dspsr/run_dspsr.sh
+++ dspsr/run_dspsr.sh
@@ -6,7 +6,7 @@
 data_dir="/home/data/C1"
 dates=("20241222" "20250220")
 fits_pattern="NGC6517_tracking-M01_*.fits"
-
+minX_MB="1024"
 PSRname="c1"
 thread="1"
 parfile="J2338+4818_tdb.par"
@@ -22,7 +22,7 @@ dspsr_commands="dspsr.txt"
 
 # Loop through all date directories
 for day in "${dates[@]}"; do
-    echo dspsr -cont -t "${thread}" -E "${parfile}" -F "${nchan}" -b "${nbin}" -K -L "${nsub}" -A -d "${npol}" -O "${PSRname}_${date}" "${data_dir}/${day}/${fits_pattern}" >> "$dspsr_commands"
+    echo dspsr -cont -U "${minX_MB}" -t "${thread}" -E "${parfile}" -F "${nchan}" -b "${nbin}" -K -L "${nsub}" -A -d "${npol}" -O "${PSRname}_${day}" "${data_dir}/${day}/${fits_pattern}" >> "$dspsr_commands"
 done                                                                                                                                                                                       echo "The dspsr.txt have generated." 
 
 # Run rfifind commands in parallel
